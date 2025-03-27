dbe6b2a996388e875871edd2f9499b720840f04f
devPach4545 <dhaivatpachchigar@gmail.com>
2025-03-26 20:18:20 -0700

    pray it works

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,3 @@
 export const getImageUrl = (path) => {
-  // Get the base URL dynamically
-  
-  const baseUrl = import.meta.env.BASE_URL || '/';
-  return `${baseUrl}assets/${path}`;
+  return "/assets/" + path;
 };
